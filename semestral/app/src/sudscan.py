c9ee481f615e042a4f2e20769db21b700df650fe
Tomáš Motus <motustom@fit.cvut.cz>
2025-05-28 18:02:11 +0200

    link gone

--- semestral/app/src/sudscan.py
+++ semestral/app/src/sudscan.py
@@ -33,23 +33,23 @@ def scan_table(image_path):
     import tarfile
     import os
 
-    urls = {
-        "det": "https://paddleocr.bj.bcebos.com/ppocr_v4/en/en_PP-OCRv4_det_infer.tar",
-        "rec": "https://paddleocr.bj.bcebos.com/ppocr_v4/en/en_PP-OCRv4_rec_infer.tar",
-        "cls": "https://paddleocr.bj.bcebos.com/ppocr_v4/ch/ch_ppocr_mobile_v2.0_cls_infer.tar"  # optional
-    }
+    #urls = {
+    #    "det": "https://paddleocr.bj.bcebos.com/ppocr_v4/en/en_PP-OCRv4_det_infer.tar",
+    #    "rec": "https://paddleocr.bj.bcebos.com/ppocr_v4/en/en_PP-OCRv4_rec_infer.tar",
+    #    "cls": "https://paddleocr.bj.bcebos.com/ppocr_v4/ch/ch_ppocr_mobile_v2.0_cls_infer.tar"  # optional
+    #}
     
-    output_dir = "./paddleocr_models"
-    os.makedirs(output_dir, exist_ok=True)
+    #output_dir = "./paddleocr_models"
+    #os.makedirs(output_dir, exist_ok=True)
     
-    for name, url in urls.items():
-        print(f"Downloading {name} model...")
-        tar_path = os.path.join(output_dir, f"{name}.tar")
-        urllib.request.urlretrieve(url, tar_path)
+    #for name, url in urls.items():
+    #    print(f"Downloading {name} model...")
+    #    tar_path = os.path.join(output_dir, f"{name}.tar")
+    #    urllib.request.urlretrieve(url, tar_path)
 
-        with tarfile.open(tar_path, "r") as tar:
-            tar.extractall(path=output_dir)
-        os.remove(tar_path)
+    #    with tarfile.open(tar_path, "r") as tar:
+    #        tar.extractall(path=output_dir)
+    #    os.remove(tar_path)
 
     from paddleocr import PaddleOCR
     ocr = PaddleOCR(use_textline_orientation=False, lang='en',
