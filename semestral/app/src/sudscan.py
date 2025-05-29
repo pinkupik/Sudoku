d541335de769b16c65f8fd415ec3ff0bfcae39e5
Tomáš Motus <motustom@fit.cvut.cz>
2025-05-29 18:31:27 +0200

    not fixes

--- semestral/app/src/sudscan.py
+++ semestral/app/src/sudscan.py
@@ -29,30 +29,9 @@ def scan_table(image_path):
           consecutive columns
         - Detected text outside the 9x9 grid boundaries is ignored
     """
-    import urllib.request
-    import tarfile
-    import os
 
-    #urls = {
-    #    "det": "https://paddleocr.bj.bcebos.com/ppocr_v4/en/en_PP-OCRv4_det_infer.tar",
-    #    "rec": "https://paddleocr.bj.bcebos.com/ppocr_v4/en/en_PP-OCRv4_rec_infer.tar",
-    #    "cls": "https://paddleocr.bj.bcebos.com/ppocr_v4/ch/ch_ppocr_mobile_v2.0_cls_infer.tar"  # optional
-    #}
-    
-    #output_dir = "./paddleocr_models"
-    #os.makedirs(output_dir, exist_ok=True)
-    
-    #for name, url in urls.items():
-    #    print(f"Downloading {name} model...")
-    #    tar_path = os.path.join(output_dir, f"{name}.tar")
-    #    urllib.request.urlretrieve(url, tar_path)
-
-    #    with tarfile.open(tar_path, "r") as tar:
-    #        tar.extractall(path=output_dir)
-    #    os.remove(tar_path)
-
-    from paddleocr import PaddleOCR
-    ocr = PaddleOCR(use_textline_orientation=False, lang='en',
+    import paddleocr
+    ocr = paddleocr.PaddleOCR(use_textline_orientation=False, lang='en',
                     text_detection_model_dir="app/official_models/PP-OCRv5_mobile_det",
                     text_recognition_model_dir="app/official_models/PP-OCRv5_mobile_rec",
                     doc_orientation_classify_model_dir="app/official_models/PP-LCNet_x1_0_doc_ori",
