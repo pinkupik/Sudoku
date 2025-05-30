f6ef2e8c36a71aaba161cd484bf96618408f7085
Tomáš Motus <motustom@fit.cvut.cz>
2025-05-30 17:20:26 +0200

    remake

--- app/src/sudscan.py
+++ app/src/sudscan.py
@@ -5,7 +5,6 @@ This module provides functionality to process images containing Sudoku puzzles
 and convert them into numerical matrices using PaddleOCR for text recognition.
 """
 import numpy as np
-from paddleocr import PaddleOCR
 import cv2
 
 
@@ -30,6 +29,7 @@ def scan_table(image_path):
           consecutive columns
         - Detected text outside the 9x9 grid boundaries is ignored
     """
+    from paddleocr import PaddleOCR
     ocr = PaddleOCR(use_textline_orientation=False, lang='en',
                     text_detection_model_dir="app/official_models/PP-OCRv5_mobile_det",
                     text_recognition_model_dir="app/official_models/PP-OCRv5_mobile_rec",
